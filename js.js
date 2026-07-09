653f89c5d59294e87a9be2347baad64d51945777
Pakhomov Dmitry  QA <WO1OFF@yandex.ru>
2026-07-09 22:13:57 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const credoButton = document.getElementById("credoButton");
+
+if (credoButton) {
+  credoButton.addEventListener("click", () => {
+    alert("Качество — это не случайность, а результат внимания к деталям.");
+  });
+}
