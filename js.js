953fb24673c3ea058ab889ceec419a336afaf7f2
Pakhomov Dmitry  QA <WO1OFF@yandex.ru>
2026-07-10 23:34:35 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,6 +2,6 @@ const credoButton = document.getElementById("credoButton");
 
 if (credoButton) {
   credoButton.addEventListener("click", () => {
-    alert(«Качество — это ответственность всей команды, моя роль — помочь его обеспечить»);
+    alert("Качество — это ответственность всей команды, моя роль — помочь его обеспечить.");
   });
 }
